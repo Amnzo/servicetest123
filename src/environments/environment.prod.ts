e4ed3c7323b6b9069eb529f53f2df79af6246bd5
Your NameAmnzo <salmi.ensa.ilsi@gmail.com>
2018-12-24 18:45:55 +0100

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCNmFO5zk0qfVSJlJeo4cfoLE8SipCVbdA",
-    authDomain: "task-a8967.firebaseapp.com",
-    databaseURL: "https://task-a8967.firebaseio.com",
-    projectId: "task-a8967",
-    storageBucket: "task-a8967.appspot.com",
-    messagingSenderId: "802765055060"
-  }
+  production: true
 };
